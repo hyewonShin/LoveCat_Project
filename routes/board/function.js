4eac5da3ed5c47ae6b4efecc2d2341e2613316eb
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2022-12-22 16:52:30 +0900

    게시글 조회 API
    전체조회, 카테고리별 조회, 게시글 상세조회

--- routes/board/function.js
+++ routes/board/function.js
@@ -1,20 +1,55 @@
 const MariaQuery = require("../../middlewares/mariaModule");
 
-const SelectUser = () => {
+const SelectAll = () => {
   return new Promise(async (resolve, reject) => {
     try {
-      console.log("SelectUser() 진입 >>> ");
-      let sql = `select u_number from user where id = "qwer"`;
+      console.log("SelectAll() 진입 >>> ");
+      let sql = `select * from board`;
       let rows = await MariaQuery(sql);
       console.log("rows", rows);
       resolve(rows);
     } catch (error) {
-      console.log("SelectUser 함수 에러", error);
-      reject("SelectUser() 에러");
+      console.log("SelectAll 함수 에러", error);
+      reject("SelectAll() 에러");
+    }
+  });
+};
+
+const SelectCategory = (category) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      console.log("SelectCategory() 진입 >>> ");
+      console.log("category >>> ", category);
+      let sql = `select * from board where category_num = "${category}"`;
+      let rows = await MariaQuery(sql);
+      console.log("rows", rows);
+      resolve(rows);
+    } catch (error) {
+      console.log("SelectCategory 함수 에러", error);
+      reject("SelectCategory() 에러");
+    }
+  });
+};
+
+const SelectBoard = (category, board_num) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      console.log("SelectBoard() 진입 >>> ");
+      console.log("category >>> ", category);
+      console.log("board_num >>> ", board_num);
+      let sql = `select * from board where category_num = "${category}" and board_num = ${board_num}`;
+      let rows = await MariaQuery(sql);
+      console.log("rows", rows);
+      resolve(rows);
+    } catch (error) {
+      console.log("SelectBoard 함수 에러", error);
+      reject("SelectBoard() 에러");
     }
   });
 };
 
 module.exports = {
-  SelectUser,
+  SelectAll,
+  SelectBoard,
+  SelectCategory,
 };
--- routes/board/index.js
+++ routes/board/index.js
@@ -1,12 +1,44 @@
 const express = require("express");
 const router = express.Router();
 const { VerifyToken } = require("../../middlewares/auth-middlewares");
-const { SelectUser } = require("./function");
+const { SelectAll, SelectCategory, SelectBoard } = require("./function");
 
-// 토큰만료/재생성 테스트
-router.get("/select", VerifyToken, async (req, res, next) => {
-  console.log("/select 진입 >> ");
-  SelectUser()
+// 전체글 조회
+router.get("/", async (req, res, next) => {
+  SelectAll()
+    .then((result) => {
+      res.status(200).json({ success: true, list: result });
+    })
+    .catch((error) =>
+      res.status(error).json({ success: false, message: error })
+    );
+});
+
+// 특정 카테고리 글 조회
+router.get("/:category", async (req, res, next) => {
+  console.log("/board/:category 진입 >> ");
+  console.log("category >> ", req.params.category);
+  let category = req.params.category;
+  SelectCategory(category)
+    .then((result) => {
+      res.status(200).json({ success: true, list: result });
+    })
+    .catch((error) =>
+      res.status(error).json({ success: false, message: error })
+    );
+});
+
+// 특정 카테고리 글 상세조회
+router.get("/:category/:board_num", async (req, res, next) => {
+  console.log("/:category/:board_num 진입 >> ");
+  console.log("category >> ", req.params.category);
+  let category = req.params.category;
+  console.log("board_num >> ", req.params.board_num);
+  let board_num = req.params.board_num;
+  console.log("!!board_num >> ", board_num);
+  console.log("헤헿 > > ", category, ", ", board_num);
+
+  SelectBoard(category, board_num)
     .then((result) => {
       res.status(200).json({ success: true, list: result });
     })
