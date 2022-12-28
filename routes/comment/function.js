cdcbc7a220e29f99deb9379b4d92e4845631c138
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2022-12-28 20:00:06 +0900

    댓글 조회

--- routes/comment/function.js
+++ routes/comment/function.js
@@ -1,3 +1,33 @@
 const MariaQuery = require("../../middlewares/mariaModule");
 
-module.exports = {};
+// 댓글 조회
+const SelectComment = (params) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      console.log("SelectComment() 진입 >>> ", params);
+      let board_num = params.board_num;
+      let comment_num = params.comment_num;
+      console.log("board_num, comment_num >>", board_num, comment_num);
+
+      //원댓글과 자식댓글들 출력
+      let sql = `SELECT * FROM comment
+                  WHERE
+                 board_num = ${board_num}
+                 AND comment_num = ${comment_num}
+                  OR board_num = ${board_num}
+                 AND p_num = ${comment_num}
+                 ORDER BY create_date ASC`;
+
+      let rows = await MariaQuery(sql);
+      console.log("rows", rows);
+      resolve(rows);
+    } catch (error) {
+      console.log("SelectComment 함수 에러", error);
+      reject("SelectComment() 에러");
+    }
+  });
+};
+
+module.exports = {
+  SelectComment,
+};
--- routes/comment/index.js
+++ routes/comment/index.js
@@ -3,10 +3,22 @@ const router = express.Router();
 const {
   VerifyToken,
   GetNickName,
+  CheckAdmin,
 } = require("../../middlewares/auth-middlewares");
 
-router.get("/", async (req, res, next) => {
-  console.log("comment 라우터 진입");
+const { SelectComment } = require("./function");
+
+// 댓글 조회
+router.get("/:board_num/:comment_num", async (req, res, next) => {
+  console.log("댓글조회API 진입 >> ");
+
+  SelectComment(req.params)
+    .then((result) => {
+      res.status(200).json({ success: true, list: result });
+    })
+    .catch((error) =>
+      res.status(error).json({ success: false, message: error })
+    );
 });
 
 module.exports = router;
