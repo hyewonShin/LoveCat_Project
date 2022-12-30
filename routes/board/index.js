766630d17125e24c469c6ccae92d60f1ad672554
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2022-12-30 16:29:54 +0900

    multer을 사용한 이미지 업로드 테스트
    추후 AWS-S3로 변환 예정

--- routes/board/index.js
+++ routes/board/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { upload } = require("../../middlewares/multer");
 const {
   VerifyToken,
   GetNickName,
@@ -61,7 +62,7 @@ router.get("/posts/:category/:board_num", async (req, res, next) => {
 });
 
 // 게시글 작성
-router.post("/", VerifyToken, async (req, res, next) => {
+router.post("/", VerifyToken, upload.single("img"), async (req, res, next) => {
   console.log("글작성API 진입 >> ", req.body);
 
   GetNickName(req.headers.authorization)
@@ -72,6 +73,33 @@ router.post("/", VerifyToken, async (req, res, next) => {
     .catch((error) =>
       res.status(error).json({ success: false, message: error })
     );
+});
+
+// 이미지 업로드
+router.post("/image", upload.single("img"), async (req, res, next) => {
+  console.log("이미지업로드 API 진입 >> ");
+
+  const {
+    fieldname,
+    originalname,
+    encoding,
+    mimetype,
+    destination,
+    filename,
+    path,
+    size,
+  } = req.file;
+
+  console.log("폼에 정의된 필드명 : ", fieldname);
+  console.log("사용자가 업로드한 파일 명 : ", originalname);
+  console.log("파일의 엔코딩 타입 : ", encoding);
+  console.log("파일의 Mime 타입 : ", mimetype);
+  console.log("파일이 저장된 폴더 : ", destination);
+  console.log("destinatin에 저장된 파일 명 : ", filename);
+  console.log("업로드된 파일의 전체 경로 ", path);
+  console.log("파일의 바이트(byte 사이즈)", size);
+
+  res.json({ ok: true, data: "Single Upload Ok" });
 });
 
 // 게시글 수정
