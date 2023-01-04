761e36e67173e71b4c271cd5315d2b07d227f7ba
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2023-01-04 16:34:47 +0900

    알림기능 스키마 생성

---
+++ schemas/Alarm.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const Alarm = new Schema(
+  {
+    id: {
+      type: String,
+    },
+    user_num: {
+      type: String,
+    },
+    nickname: {
+      type: String,
+    },
+    chat_id: {
+      type: String,
+    },
+    title: {
+      type: String,
+    },
+    days: {
+      type: String,
+    },
+    markAsRead: {
+      type: String,
+    },
+  },
+  {
+    versionKey: false,
+    _id: true,
+    timestamps: true,
+  }
+);
+
+const UserInfo = new Schema(
+  {
+    user_num: {
+      type: String,
+    },
+    nickname: {
+      type: String,
+    },
+    chat_id: {
+      type: String,
+    },
+  },
+  {
+    versionKey: false,
+    _id: true,
+    timestamps: true,
+  }
+);
+
+const Alarm_log = mongoose.model("Alarm", Alarm, "Alarm_log");
+const UserInfo_log = mongoose.model("UserInfo", UserInfo, "UserInfo_log");
+
+module.exports = { Alarm_log, UserInfo_log };
